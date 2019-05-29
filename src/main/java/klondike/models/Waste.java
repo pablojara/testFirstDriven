a8ba71b04fa899ee35ce25c034ebf6dcb452e969
pablo <jaramillodominguezpablo@gmail.com>
2019-05-29 21:30:41 +0200

    Remove inheritance from CardStack in Waste

--- src/main/java/klondike/models/Waste.java
+++ src/main/java/klondike/models/Waste.java
@@ -1,4 +1,28 @@
 package klondike.models;
 
-public class Waste extends CardStack {
+import java.util.Stack;
+
+public class Waste {
+	
+    protected Stack<Card> cards;
+
+    public Waste() {
+        this.cards = new Stack<Card>();
+    }
+	
+    public boolean empty() {
+        return this.cards.empty();
+    }
+
+    public Card peek() {
+        return this.cards.peek();
+    }
+
+    public Card pop() {
+        return this.cards.pop();
+    }
+
+    public void push(Card card) {
+        this.cards.push(card);
+    }
 }
--- src/test/java/klondike/models/WasteTest.java
+++ src/test/java/klondike/models/WasteTest.java
@@ -1,10 +1,72 @@
 package klondike.models;
 
-public class WasteTest extends CardStackTest {
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
-	@Override
-	protected CardStack createCardStack() {
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+
+import klondike.models.builders.CardBuilder;
+
+public class WasteTest {
+
+	private Waste createWaste() {
 		return new Waste();
+	}
+	
+	private List<Card> getCards(){
+		List<Card> cards = new ArrayList<Card>();
+		cards.add(new CardBuilder().build());
+		cards.add(new CardBuilder().build());
+		return cards;
+	}
+	
+	@Test
+	public void testEmptyWithEmpty() {
+		Waste waste = this.createWaste();
+		assertTrue(waste.empty());
+	}
+	
+	@Test
+	public void testEmptyWithNotEmpty() {
+		Waste waste = this.createWaste();
+		waste.push(this.getCards().get(0));
+		assertFalse(waste.empty());
+	}
+
+	@Test
+	public void testPushWithEmpty() {
+		Waste waste = this.createWaste();
+		waste.push(this.getCards().get(0));
+		assertEquals(this.getCards().get(0), waste.peek());
+	}
+	
+	@Test
+	public void testPushWithNotEmpty() {
+		Waste waste = this.createWaste();
+		waste.push(this.getCards().get(0));
+		waste.push(this.getCards().get(1));
+		assertEquals(this.getCards().get(1), waste.peek());
+	}
+
+	@Test
+	public void testPopEmpty() {
+		Waste waste = this.createWaste();
+		waste.push(this.getCards().get(0));
+		assertEquals(this.getCards().get(0), waste.pop());
+		assertTrue(waste.empty());
+	}
+	
+	@Test
+	public void testPopNotEmpty() {
+		Waste waste = this.createWaste();
+		waste.push(this.getCards().get(0));
+		waste.push(this.getCards().get(1));
+		assertEquals(this.getCards().get(1), waste.pop());
+		assertEquals(this.getCards().get(0), waste.peek());
 	}
 	
 }
